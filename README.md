5ff01a5ab655d854fca33207449be83c8a269787
Rudnei Jose da Silva Bertol Junior <rudnei.bertol@interop.com.br>
2016-09-12 11:18:31 -0300

    Ajuste de README.md e adição de diretório erratas

--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 Repositório para scripts do Katello.
 
 **satellite6_begin.sh**: Script para adicionar máquina no Katello/Satellite6 e no Puppet.
+
+**erratas**: Diretório com Scripts para administração das erratas dos CentOS no Katello e no Satellite6
+## 
+
+**tasks**: Script para remoção das tasks penduradas no servidor Katello/Satellite6
--- erratas/errata.sh
+++ erratas/errata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 USERNAME='admin'
-PASSWORD='Z4LfJ8Wxf2bPjfG2NmDMo3VihKXYbCAb'
+PASSWORD=$(grep -i ^default_password /etc/pulp/server.conf | awk '{print $2}')
 wget -N http://cefs.steve-meier.de/errata.latest.xml
 wget -N https://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml
 errata_import.pl --errata=errata.latest.xml --rhsa-oval=com.redhat.rhsa-all.xml --user=${USERNAME} --password=${PASSWORD}
