bedc4223cbb85557486e728bbf6060370a0a4c2c
Rudnei Jose da Silva Bertol Junior <rudnei.bertol@interop.com.br>
2016-09-12 11:23:55 -0300

    Modificação de tasks e erratas

--- erratas/errata.sh
+++ erratas/errata.sh
@@ -4,4 +4,3 @@ PASSWORD=$(grep -i ^default_password /etc/pulp/server.conf | awk '{print $2}')
 wget -N http://cefs.steve-meier.de/errata.latest.xml
 wget -N https://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml
 errata_import.pl --errata=errata.latest.xml --rhsa-oval=com.redhat.rhsa-all.xml --user=${USERNAME} --password=${PASSWORD}
-
--- tasks/clear-task.sh
+++ tasks/clear-task.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 STATE=""
 USER='admin'
-PWD='Z4LfJ8Wxf2bPjfG2NmDMo3VihKXYbCAb'
-pulp-admin login -u ${USER} -p ${PWD}
+PWD=$(grep -i ^default_password /etc/pulp/server.conf | awk '{print $2}')
 for TASK in `pulp-admin -u ${USER} -p ${PWD} tasks list | egrep '^Task Id:|^State:' | sed -e 's,^Task Id: ,,' -e 's,^State: ,,'`; do
         if [ "$STATE" = "" ]; then
                 STATE=$TASK
         else
                 if [ $STATE != Successful ] && [ $STATE != Cancelled ] && [ $STATE != Failed ]; then
-                        pulp-admin tasks details --task-id=$TASK
-                        pulp-admin tasks cancel --task-id=$TASK
+                        pulp-admin -u ${USER} -p ${PWD} tasks details --task-id=$TASK
+                        pulp-admin -u ${USER} -p ${PWD} tasks cancel --task-id=$TASK
                 fi
                 STATE=""
         fi
 done
-pulp-admin logout
